98f00d0f72837ad0e06289d812e650b3dfcf15d5
frmnxhken <frmnxhken@gmail.com>
2025-04-30 13:35:49 +0700

    Add base presention page & history page

---
+++ app/history/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const History = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>History Page</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default History
---
+++ app/presention/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+const Presention = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Presention Page</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Presention
