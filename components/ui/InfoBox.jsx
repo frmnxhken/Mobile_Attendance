7583031e78ff91dae54874e9efc5cbe5d06c7254
frmnxhken <frmnxhken@gmail.com>
2025-05-09 11:14:07 +0700

    Update infobox

--- components/ui/InfoBox.jsx
+++ components/ui/InfoBox.jsx
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    columnGap: 10,
     backgroundColor: "#fff",
     borderRadius: 12,
     paddingHorizontal: 20,
@@ -28,12 +29,16 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: Sizes.body,
-    fontFamily: "Inter-Medium"
+    fontFamily: "Inter-Medium",
+    maxWidth: 50,
+    minWidth: 50
   },
   value: {
     fontSize: Sizes.body,
     color: Colors.gray,
-    fontFamily: "Inter-Regular"
+    fontFamily: "Inter-Regular",
+    textAlign: "right",
+    width: "80%"
   },
 });
 
